475c2451da7dcf8595c389a8cd98ca6189fc3933
Biplab Sutradhar <biplabsutradhar1453@gmail.com>
2024-10-07 16:16:05 +0530

    created deleted message

---
+++ src/app/api/delete-masasge/[messageid]/route.ts
@@ -0,0 +1,47 @@
+import { getServerSession, User } from 'next-auth'; 
+import dbConnect from '@/libs/dbConnects';
+import UserModel from '@/model/User';
+import { authOptions } from '../../auth/[...nextauth]/options';
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { messageId: string } }
+) {
+  const messageId = params.messageId;
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user: User = session?.user as User;
+
+  if (!user || !session) {
+    return Response.json(
+      { success: false, message: 'Not authorized' },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const updateResult = await UserModel.updateOne(
+      { _id: user._id },
+      { $pull: { messages: { _id: messageId } } }
+    );
+
+    if (updateResult.modifiedCount === 0) {
+      return Response.json(
+        { success: false, message: 'Message not found' },
+        { status: 404 }
+      );
+    }
+
+    return Response.json(
+      { success: true, message: 'Message deleted successfully' },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log('Error deleting message:', error);
+
+    return Response.json(
+      { success: false, message: 'Message not found' },
+      { status: 404 }
+    );
+  }
+}
